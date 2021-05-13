32ce841664fec00631c48b9d73084c39fc7259b3
santosh-cell <santoshkolekar165@gmail.com>
2021-05-13 12:55:18 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+// connect to db
+mongoose.connect(
+process.env.DB_CONNECT,
+{ useUnifiedTopology: true, useNewUrlParser:true },
+() => console.log("connected to db")
+);
+// Import routes
+const listinRoutes = require("./routes/listing");
+// route Middlewares
+app.use(express.json())
+app.use("/api/listings", listinRoutes);
+app.listen(4000,() => console.log("server up and runing on port 4000!"));
+
